7fdc1c445c81efbb192b57ae6380b7c7c9aa218e
ziyao <419493137@qq.com>
2015-11-13 18:04:41 +0800

    构建程序架构，初始化导航栏和主页面

---
+++ ZYAnything/ZYAnything/Classes/UIDefines.h
@@ -0,0 +1,19 @@
+//
+//  UIDefines.h
+//  ZYAnything
+//
+//  Created by ZhangZiyao on 15/11/13.
+//  Copyright © 2015年 soez. All rights reserved.
+//
+
+#ifndef UIDefines_h
+#define UIDefines_h
+
+#pragma mark - 颜色
+#define RGBA(R/*红*/, G/*绿*/, B/*蓝*/, A/*透明*/) \
+[UIColor colorWithRed:R/255.f green:G/255.f blue:B/255.f alpha:A]
+
+
+
+
+#endif /* UIDefines_h */
