815f1bd205b50ababc40ba791887a583e05e17ac
ziyao <419493137@qq.com>
2015-11-13 17:51:17 +0800

    初始化项目ZYAnything

---
+++ ZYAnything/ZYAnything/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYAnything
+//
+//  Created by ZhangZiyao on 15/11/13.
+//  Copyright © 2015年 soez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYAnything/ZYAnything/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYAnything
+//
+//  Created by ZhangZiyao on 15/11/13.
+//  Copyright © 2015年 soez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
